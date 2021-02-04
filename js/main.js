69358307f873e712e7bd5b379a5832642ac58da4
Shamil Omarov <s_omarov05@mail.ru>
2021-02-04 16:05:26 +0300

    Жесткий рефакторинг кода

--- js/main.js
+++ js/main.js
@@ -1,25 +1,58 @@
 const startDate = document.querySelector('#start-date');
 const endDate = document.querySelector('#end-date');
 const go = document.querySelector('#go');
+const table = document.querySelector('#table');
 
 go.addEventListener('click', function() {
     if (!startDate.value || !endDate.value) return;
 
+    // параметры для запроса
     const params = {
         start: startDate.value,
         end: endDate.value
     }
 
+    // получаем данные о биткоине
     fetch('https://api.coindesk.com/v1/bpi/historical/close.json' + new URLSearchParams(params))
         .then(response => response.json())
         .then(response => {
             console.log('response', response);
-            
+
+            // получили данные
             const { bpi } = response;
 
-            // вывести в таблицу под формой
+            // надо вывести данные
+            renderBitcoinDate(bpi, table);
+
         })
         .catch(error => {
             console.error(error);
         });
 })
+
+function renderBitcoinDate(tableDate, element) {
+    console.log('tableDate', tableDate);
+
+    let result = 
+        `<table>
+            <tr>
+                <th>Дата</th>
+                <th>Цена</th>
+            </tr>
+        `
+    ;
+
+    for (let item in tableDate) {
+        result += 
+        `
+            <tr>
+                <td> ${ item } </td>
+                <td> ${ tableDate[item] } </td>
+            </tr>
+        `
+    }
+
+    result += '</table>';
+    
+    element.innerHTML = result;
+}
